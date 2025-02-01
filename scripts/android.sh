c7b68bb73346c411828fc8aeba4372f3f3218757
jjaareet <2613089+jjaareet@users.noreply.github.com>
2025-02-01 08:16:18 -0700

    fix: ci

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e
+set -ex
 
 #create android project with some adjustments
 
+# create android and ios directories
 yarn expo prebuild
-echo "Small Android adjustments..."
+
 # dark splash
+# todo: test better, i don't think this is working. during the build i see: 'The android project is malformed, project files will be cleared and reinitialized.'
 cp ./scripts/xml/colors.xml \
   ./android/app/src/main/res/values-night/colors.xml
--- scripts/f-droid.sh
+++ scripts/f-droid.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-set -e
+set -ex
 
 #create android project with some adjustments
 
+# set flag for f-droid (hide donation button)
+sed -i '' -e 's/FLAG_FDROID = false/FLAG_FDROID = true/' Global.tsx
+
 # small android adjustments
 ./scripts/android.sh
+
 # apply node_module patches (`rm -rf node_modules && yarn` to reverse)
 yarn patch-package --patch-dir scripts/patches
+
 # remove signing config
-sed -i -e '/signingConfig /d' android/app/build.gradle
+sed -i '' -e '/signingConfig /d' android/app/build.gradle
