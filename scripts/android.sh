11f7bd3d1af63080f94824640ebd0a431147642d
jjaareet <2613089+jjaareet@users.noreply.github.com>
2025-02-01 22:20:45 -0700

    fix: ci

--- scripts/android.sh
+++ scripts/android.sh
@@ -3,8 +3,9 @@ set -ex
 
 #create android project with some adjustments
 
+# create android and ios directories
 yarn expo prebuild
-echo "Small Android adjustments..."
+
 # dark splash
 # todo: test better, i don't think this is working. during the build i see: 'The android project is malformed, project files will be cleared and reinitialized.'
 cp ./scripts/xml/colors.xml \
