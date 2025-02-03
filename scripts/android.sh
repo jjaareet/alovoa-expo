642e3982e6d19952d0541f00b461a1a6964a8e2e
jjaareet <2613089+jjaareet@users.noreply.github.com>
2025-02-02 17:19:50 -0700

    fix: ci

--- scripts/android.sh
+++ scripts/android.sh
@@ -7,6 +7,5 @@ set -ex
 yarn expo prebuild
 
 # dark splash
-# todo: test better, i don't think this is working. during the build i see: 'The android project is malformed, project files will be cleared and reinitialized.'
 cp ./scripts/xml/colors.xml \
   ./android/app/src/main/res/values-night/colors.xml
