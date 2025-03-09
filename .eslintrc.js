4a325cb6add9a2b05fa34b1766c39f4a2b6fcc53
jjaareet <2613089+jjaareet@users.noreply.github.com>
2025-03-09 16:25:43 -0700

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
   ignorePatterns: ['/dist/*'],
   rules: {
+    'prettier/prettier': 'error',
     // todo: remove and fix warnings
     // https://react.dev/reference/react/hooks
     // https://react.dev/reference/rules/rules-of-hooks
     // https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
-    'react-hooks/exhaustive-deps': 'off'
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+// https://prettier.io/docs/options.html
+module.exports = {
+  arrowParens: 'avoid',
+  singleAttributePerLine: true,
+  singleQuote: true,
+};
